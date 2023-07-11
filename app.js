9d28f0610fec658e56543df1c3aba77fba36e1ff
Eddie Sandler <ed.sandler@gmail.com>
2023-07-11 19:34:05 -0400

    WIP

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 "use strict";
 console.log("Let's get this party started!");
+const apiKey = MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym;
+let url = 'https://api.giphy.com/v1/gifs/search';
 
+// click handler for submit button
+$('#btn_search').on("click", getGif);
+
+
+async function getGif() {
+  let gif = await axios.get('?q=&api_key=');// get (`${url}{apiKey}`)
+
+}
