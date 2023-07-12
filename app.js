31e456abe50bc8855fc167f1d69e9a900d97dda8
michael frederickson <mpfplus@gmail.com>
2023-07-11 18:26:37 -0600

    Completed main portion of exercise, awaiting code review

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 "use strict";
+
 console.log("Let's get this party started!");
+
 const apiKey = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
-let url = 'https://api.giphy.com/v1/gifs/search';
+const url = 'https://api.giphy.com/v1/gifs/search';
 
 // click handler for submit button
 $('#btn_search').on("click", getGif);
-$('#btn_remove').on("click");
+$('#btn_remove').on("click", removeGifs);
 
+
+/** Clears gif field */
+function removeGifs() {
+  $('#gifFeed').empty();
+}
+
+/** Retrieves a gif from GIPHY API, appends it to the DOM */
 async function getGif(evt) {
   evt.preventDefault();
 
@@ -14,11 +23,10 @@ async function getGif(evt) {
   let $gifFeed = $('#gif_feed');
 
   let gifResponse = await axios.get(url, { params: { q: search, api_key: apiKey } });
-  console.log(gifResponse.data);
   let result = gifResponse.data.data[0].images.original.url;
   let $image = $(`<img src= ${result}>`);
 
-
   $gifFeed.append($image);
 
+  $('#bar_search').val('');
 }
