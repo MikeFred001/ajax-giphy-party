410c54c529e64e8592da416524a4e0c0ad6f917f
michael frederickson <mpfplus@gmail.com>
2023-07-11 18:41:41 -0600

    Making structural changes following code review

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ console.log("Let's get this party started!");
 
 const apiKey = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
 const url = 'https://api.giphy.com/v1/gifs/search';
+// global gifFeed
 
 // click handler for submit button
 $('#btn_search').on("click", getGif);
@@ -11,19 +12,21 @@ $('#btn_remove').on("click", removeGifs);
 
 
 /** Clears gif field */
-function removeGifs() {
-  $('#gifFeed').empty();
+function removeGifs(evt) {
+  evt.preventDefault();
+  $('#gif_feed').empty();
 }
 
 /** Retrieves a gif from GIPHY API, appends it to the DOM */
+// Functional decomposition
 async function getGif(evt) {
   evt.preventDefault();
 
-  let search = $('#bar_search').val();
+  let search = $('#bar_search').val(); // Better variable name
   let $gifFeed = $('#gif_feed');
 
   let gifResponse = await axios.get(url, { params: { q: search, api_key: apiKey } });
-  let result = gifResponse.data.data[0].images.original.url;
+  let result = gifResponse.data.data[0].images.original.url; // gifResult? Something better
   let $image = $(`<img src= ${result}>`);
 
   $gifFeed.append($image);
