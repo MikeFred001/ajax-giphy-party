28ee9c1680b70b2fcc69534012658e742eb99e23
michael frederickson <mpfplus@gmail.com>
2023-07-11 18:08:05 -0600

    WIP: need to convert returned img url to img tag

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 "use strict";
 console.log("Let's get this party started!");
-const apiKey = MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym;
+const apiKey = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
 let url = 'https://api.giphy.com/v1/gifs/search';
 
 // click handler for submit button
 $('#btn_search').on("click", getGif);
+$('#btn_remove').on("click")
 
+async function getGif(evt) {
+  evt.preventDefault();
 
-async function getGif() {
-  let gif = await axios.get('?q=&api_key=');// get (`${url}{apiKey}`)
+  let search = $('#bar_search').val();
+  let $gifFeed = $('#gif_feed');
 
+  let gifResponse = await axios.get(url, {params: {q: search, api_key: apiKey}});
+  console.log(gifResponse.data);
+  $gifFeed.append(gifResponse.data.data[0].images.original.url);
 }
